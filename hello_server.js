1d11866438ea8d20d0df5902266362970ffb1793
Josh Kuhar <jdkuhar@gmail.com>
2016-08-18 09:22:29 -0400

    initial commit

---
+++ hello_server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send("Hello World");
+});
+
+app.get('/:firstname/:lastname', function(request, response) {
+	var first = request.params.firstname;
+	var last = request.params.lastname;
+	response.send(["Hello", first, last].join(" "));
+});
+
+app.get('/test', function(request, response){
+	response.send("I worked");
+});
+
+app.get('/jedi/:firstname/:lastname', function(request, response) {
+	var first = request.params.firstname;
+	var last = request.params.lastname;
+	response.send(["Hello", last.substring(0, 2) + first.substring(0, 3)].join(" "));
+});
+
+
+app.listen(8080);
+
